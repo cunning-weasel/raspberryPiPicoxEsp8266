13ab3df7691f57acbff31ba09370e9b2fbeb21d9
weasel <paulbongers22@gmail.com>
2023-01-16 21:31:16 +0100

    udpated readme

--- README.md
+++ README.md
@@ -1,8 +1,23 @@
 # raspberryPiPico x Esp8266
-setup of raspberry pi pico (non wifi) connecting up to the ESP8266 12E D1 Mini. 
+setup of raspberry pi pico w & ESP8266 12E D1 Mini. 
 
-ESP8266 acts as the access point for picoo to run a c based web server.
+Set up the ESP8266:
 
-For the moment, the MVP should be able to process and store temperature readings - and process this in a multi-threaded way. 
+Flash the ESP8266 with NodeMCU firmware.
+Connect the ESP8266 to network and obtain its IP address.
+Configure the UART communication on the ESP8266, set the baud rate and use the software serial library to communicate over UART.
+Write the js code that will run on the ESP8266 to handle the Telegram bot functionality. This code should use the Telegram Bot API to interact with the Telegram server, handle incoming messages and commands, and send them to the Raspberry Pi Pico over UART.
 
-Ultimately, the devices are intended to be used to pick up blackouts, notify users via telegram/ email that there's an electricity cut, and the device should trigger a set of instructions so as to time the blackout, keep a log of this data and predict when and how long the next blackout will be on an ongoing basis. 
+Set up the Raspberry Pi Pico:
+
+Connect the Raspberry Pi Pico to network.
+Configure the UART communication on the Raspberry Pi Pico, set the baud rate and use the RP2040 Microcontroller Firmware for the UART communication.
+Write the C code that will run on the Raspberry Pi Pico to handle the task management functionality. This code should handle the storage of commands, send reminders, and handle tagging other users.
+
+Connect the ESP8266 and the Raspberry Pi Pico:
+
+~~Connect the ESP8266 and the Raspberry Pi Pico via jumper cables on a breadboard.~~
+Have the ESP8266 forward incoming commands from the Telegram bot to the Raspberry Pi Pico over UART.
+Have the Raspberry Pi Pico send reminders and other commands back to the ESP8266 over UART to be sent to the Telegram bot.
+
+
--- main.c
+++ main.c
@@ -3,104 +3,13 @@
 #include "hardware/uart.h"
 #include "hardware/timer.h"
 #include "hardware/regs/dreq.h"
+#include <sys/types.h>
+#include <unistd.h>
 
 #define UART_ID uart0
 #define BAUD_RATE 115200
 
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <unistd.h>
-
-// Function for configuring the ESP8266 as a SoftAP+station
-void config_esp8266(int sockfd)
-{
-    char buffer[256];
-    int n;
-
-    // Configure the ESP8266 as a SoftAP+station
-    sprintf(buffer, "AT+CWMODE=3\r\n");
-    n = write(sockfd, buffer, strlen(buffer));
-    if (n < 0)
-    {
-        perror("ERROR writing to socket");
-        return;
-    }
-    usleep(1000000); // Wait 1 second
-
-    // Set the SoftAP name
-    sprintf(buffer, "AT+CWSAP=\"pos_softap\",\"\",11,0,3\r\n");
-    n = write(sockfd, buffer, strlen(buffer));
-    if (n < 0)
-    {
-        perror("ERROR writing to socket");
-        return;
-    }
-    usleep(1000000); // Wait 1 second
-
-    // Enable multi-connection mode
-    sprintf(buffer, "AT+CIPMUX=1\r\n");
-    n = write(sockfd, buffer, strlen(buffer));
-    if (n < 0)
-    {
-        perror("ERROR writing to socket");
-        return;
-    }
-    usleep(1000000); // Wait 1 second
-}
-
-// Main function
 int main(void)
 {
-    // Socket descriptor and new socket descriptor
-    int sockfd, new_sockfd;
-    // Port number and client length
-    int portno, clilen;
-    // Server and client addresses
-    struct sockaddr_in serv_addr, cli_addr;
-    // Buffer for storing data
-    char buffer[256];
-    // Number of bytes read or written
-    int n;
-
-    // Create the socket
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0)
-    {
-        perror("ERROR opening socket");
-        return 1;
-    }
-
-    // Read the temperature sensor value
-    int sensor_temp = 4;
-    int conversion_factor = 3.3 / 65535;
-    int reading_temp = read_u16(sensor_temp) * conversion_factor;
-    float temperature = 27 - (reading_temp - 0.706) / 0.001721;
-
-    // Send the temperature value in an HTML page to the client
-    char val[256];
-    sprintf(val, "<head><title>Pi Pico Server</title></head><body><p>Temperature: %d deg</p></body>", (int)temperature);
-    int length = strlen(val);
-    sprintf(buffer, "AT+CIPSEND=1,%d\r\n", length);
-    n = write(new_sockfd, buffer, strlen(buffer));
-    if (n < 0)
-    {
-        perror("ERROR writing to socket");
-        return 1;
-    }
-    bzero(buffer, 256);
-    n = read(new_sockfd, buffer, 255);
-    if (n < 0)
-    {
-        perror("ERROR reading from socket");
-        return 1;
-    }
-    printf("Data sent: %s\n", buffer);
-    n = write(new_sockfd, val, strlen(val));
-    if (n < 0)
-    {
-        perror("ERROR writing to socket");
-        return 1;
-    }
+    
 }
