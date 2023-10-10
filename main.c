94b5e39ec70b36d0762f80e902fe33b57f5d390e
weasel <paulbongers22@gmail.com>
2023-10-10 22:21:13 +0200

    rm string.h

--- main.c
+++ main.c
@@ -1,14 +1,56 @@
 #include "pico/stdlib.h"
 #include "hardware/uart.h"
 #include <stdio.h>
-#include <string.h>
 
 #define BAUD_RATE 115200
 #define UART_ID uart0
 
+// std strlen trashes cache
+size_t weasel_strlen(char *string)
+{
+    char *p = string;
+    size_t len = 0;
+
+    while (*p)
+    {
+        len++;
+        p++;
+    }
+    return len;
+}
+
+// cache a single item
+size_t custom_strlen_cacher(char *str)
+{
+    static char *start = NULL;
+    static char *end = NULL;
+    size_t len = 0;
+    size_t cap = 20000; // 20kb
+
+    // str is cached and current pointer is within it
+    if (start && str >= start && str <= end)
+    {
+        // calc new strlen
+        len = end - str;
+        return len;
+    }
+
+    // calc actual length
+    len = weasel_strlen(str);
+
+    // do cache
+    if (len > cap)
+    {
+        start = str;
+        end = str + len;
+    }
+    // un-cached return
+    return len;
+}
+
 // function to receive data from the ESP8266 via UART
 void uartSerialRxMonitor(char *command, char *res) {
-    int command_len = strlen(command);
+    int command_len = custom_strlen_cacher(command);
     int res_len = 0;
 
     while (uart_any() > 0) {
@@ -33,13 +75,13 @@ int main() {
 
     // configure as SoftAP+station mode
     char command_CWMODE[] = "AT+CWMODE=3";
-    uart_write(command_CWMODE, strlen(command_CWMODE));
+    uart_write(command_CWMODE, custom_strlen_cacher(command_CWMODE));
     uart_write("\r\n", 2);
     delay_ms(1000);
 
     // set SoftAP name
     char command_CWSAP[] = "AT+CWSAP=" pos_softap "," ",11,0,3";
-    uart_write(command_CWSAP, strlen(command_CWSAP));
+    uart_write(command_CWSAP, custom_strlen_cacher(command_CWSAP));
     uart_write("\r\n", 2);
     delay_ms(1000);
     char res[20];
@@ -48,7 +90,7 @@ int main() {
 
     // enable multi connection mode
     char command_CIPMUX[] = "AT+CIPMUX=1";
-    uart_write(command_CIPMUX, strlen(command_CIPMUX));
+    uart_write(command_CIPMUX, custom_strlen_cacher(command_CIPMUX));
     uart_write("\r\n", 2);
     delay_ms(1000);
     uartSerialRxMonitor(command_CIPMUX, res);
@@ -56,7 +98,7 @@ int main() {
 
     // enable the TCP server with port 80
     char command_CIPSERVER[] = "AT+CIPSERVER=1,80";
-    uart_write(command_CIPSERVER, strlen(command_CIPSERVER));
+    uart_write(command_CIPSERVER, custom_strlen_cacher(command_CIPSERVER));
     uart_write("\r\n", 2);
     delay_ms(2000);
     uartSerialRxMonitor(command_CIPSERVER, res);
@@ -80,18 +122,18 @@ int main() {
 
         printf("%s\n", val);
         char length[5];
-        sprintf(length, "%d", strlen(val));
+        sprintf(length, "%d", custom_strlen_cacher(val));
 
         char command_CIPSEND[] = "AT+CIPSEND=1,";
         // concatenate length string to the send string
         strcat(command_CIPSEND, length);
-        uart_write(command_CIPSEND, strlen(command_CIPSEND));
+        uart_write(command_CIPSEND, custom_strlen_cacher(command_CIPSEND));
         uart_write("\r\n", 2);
         delay_ms(2000);
         uartSerialRxMonitor(command_CIPSEND, res);
         printf("Data sent-> %s\n", res);
 
-        uart_write(val, strlen(val));
+        uart_write(val, custom_strlen_cacher(val));
         uart_write("\r\n", 2);
         delay_ms(10000);
     }
